adba85a526ad558123baa401b8da877464192bb9
Laksh Nanwani <lakshanshul@gmail.com>
2025-06-29 02:33:28 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from setuptools import setup
 
 package_name = 'human_3d_detector'
@@ -7,17 +8,15 @@ setup(
     version='0.1.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Your Name',
-    maintainer_email='your_email@domain.com',
-    description='Human detection with YOLO and depth image using ROS2',
-    license='Apache License 2.0',
-    tests_require=['pytest'],
+    maintainer_email='you@domain.com',
+    description='Human 3D detection with YOLO and depth image',
+    license='Apache-2.0',
     entry_points={
         'console_scripts': [
             'human_detector_node = human_3d_detector.human_detector_node:main'
